b2fe78410d56b7ab784980314f3f71292c6441e9
RiverAndBoat <842714673@qq.com>
2020-07-24 14:47:32 +0800

    [JiangYuzhou] feat: Finish BooleanOperatorsTest

--- src/test/java/com/twc/javaBasic/BooleanOperatorsTest.java
+++ src/test/java/com/twc/javaBasic/BooleanOperatorsTest.java
@@ -6,33 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BooleanOperatorsTest {
-  @SuppressWarnings({"PointlessBooleanExpression", "ConstantConditions"})
-  @Test
-  void should_perform_logical_boolean_operations() {
-    boolean[] actualResults = {
-      true && true,
-      true && false,
-      false && false,
-      true || true,
-      true || false,
-      false || false,
-      true & true,
-      true & false,
-      false & false,
-      true | true,
-      true | false,
-      false | false,
-      3 == 7,
-      4 != 5
-    };
+    @SuppressWarnings({"PointlessBooleanExpression", "ConstantConditions"})
+    @Test
+    void should_perform_logical_boolean_operations() {
+        boolean[] actualResults = {
+                true && true,
+                true && false,
+                false && false,
+                true || true,
+                true || false,
+                false || false,
+                true & true,
+                true & false,
+                false & false,
+                true | true,
+                true | false,
+                false | false,
+                3 == 7,
+                4 != 5
+        };
 
-    // TODO:
-    //  please write your answers directly(modify the value of expectedResult to equal actualResults).
-    //
-    // <--start
-    boolean[] expectedResult = {};
-    // --end-->
+        // TODO:
+        //  please write your answers directly(modify the value of expectedResult to equal actualResults).
+        //
+        // <--start
+        boolean[] expectedResult = {true, false, false, true, true, false, true, false, false, true, true, false, false, true};
+        // --end-->
 
-    assertArrayEquals(expectedResult, actualResults);
-  }
+        assertArrayEquals(expectedResult, actualResults);
+    }
 }
